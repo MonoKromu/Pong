9718fc45d27167f5a755e8464c1a8ba51301c4df
MonoKromu <naydanovss@mail.ru>
2025-01-11 17:03:05 +0700

    Worker + UDPServer behavior (test)

--- server/src/UDPServer.java
+++ server/src/UDPServer.java
@@ -1,19 +1,30 @@
+import com.google.gson.Gson;
+import domain.Worker;
+import dtos.Action;
+import dtos.GameState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.HashMap;
 
 public class UDPServer {
     private static final Logger logger = LoggerFactory.getLogger(UDPServer.class);
     private static final int serverPort = 8000;
+    private static HashMap<Integer, Worker> workers = new HashMap<>();
+    private static HashMap<Integer, GameState> states = new HashMap<>();
+    public static DatagramSocket serverSocket;
 
     public static void init() {
         new Thread(() -> {
-            try (DatagramSocket serverSocket = new DatagramSocket(serverPort)) {
+            try {
+                Gson gson = new Gson();
+                serverSocket = new DatagramSocket(serverPort);
                 logger.info("Server started on port {}", serverPort);
 
-                byte[] receiveData = new byte[1024];
+                byte[] receiveData = new byte[512];
 
                 while (true) {
                     DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -21,18 +32,32 @@ public class UDPServer {
                     serverSocket.receive(receivePacket);
 
                     String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                    System.out.println("Получено сообщение: " + receivedMessage);
-
-                    // ответ клиенту
-                    String response = "Сообщение получено: " + receivedMessage;
-                    byte[] sendData = response.getBytes();
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
-                    serverSocket.send(sendPacket);
+                    Action action = gson.fromJson(receivedMessage, Action.class);
+                    if(action.key=='n') {
+                        states.put(action.id, new GameState());
+                        workers.put(action.id, new Worker(states.get(action.id), () -> {
+                            GameState state = states.get(action.id);
+                            byte[] out = gson.toJson(state).getBytes();
+                            DatagramPacket packet = new DatagramPacket(out, out.length, receivePacket.getAddress(), receivePacket.getPort());
+                            try {
+                                serverSocket.send(packet);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }));
+                    }
+                    else if(action.key == 'w' || action.key == 's'){
+                        workers.get(action.id).update(action);
+                    }
+                    //String response = "Сообщение получено: " + receivedMessage;
+                    //byte[] sendData = response.getBytes();
+                    //DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+                    //serverSocket.send(sendPacket);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }).start();
-
     }
 }
+
---
+++ server/src/dtos/GameState.java
@@ -0,0 +1,7 @@
+package dtos;
+
+public class GameState {
+    public int plank1, plank2;
+    public double ballX, ballY;
+    public int plank1Points, plank2Points;
+}
