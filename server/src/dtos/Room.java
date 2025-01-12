972e0ebec9f1212fe1dac8c2739ec4d16ec6a987
MonoKromu <naydanovss@mail.ru>
2025-01-13 02:58:47 +0700

    create and enter rooms

--- server/src/dtos/Room.java
+++ server/src/dtos/Room.java
@@ -1,8 +1,12 @@
 package dtos;
 
+import java.net.InetAddress;
+
 public class Room {
     public int id;
     public String name;
     public User host;
+    public InetAddress hostIP;
     public User guest;
+    public InetAddress guestIP;
 }
