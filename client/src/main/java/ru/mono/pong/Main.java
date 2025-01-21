8712c9ac0887213638d44667dee7c5786b4623e1
enoughLev <levchik-z@mail.ru>
2025-01-21 23:40:39 +0700

    release client v0.3
    need: check rooms, fix connections

--- client/src/main/java/ru/mono/pong/Main.java
+++ client/src/main/java/ru/mono/pong/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
+import java.util.Objects;
 
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("auth.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("connect.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/pong.png"))));
         stage.setTitle("Pong Masters");
         stage.setScene(scene);
         stage.setResizable(false);
@@ -21,6 +24,5 @@ public class Main extends Application {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         launch();
-
     }
 }
